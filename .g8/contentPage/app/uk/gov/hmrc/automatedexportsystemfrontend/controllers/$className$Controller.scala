e91881ece40dd97d2d86ebcd0fa2fb2a610cf34f
TJSarno <thomassarno@hotmail.com>
2026-08-14 14:24:47 +0100

    removed temporary files, updated actions

--- .g8/contentPage/app/uk/gov/hmrc/automatedexportsystemfrontend/controllers/$className$Controller.scala
+++ .g8/contentPage/app/uk/gov/hmrc/automatedexportsystemfrontend/controllers/$className$Controller.scala
@@ -3,19 +3,20 @@ package uk.gov.hmrc.automatedexportsystemfrontend.controllers
 import javax.inject.Inject
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.automatedexportsystemfrontend.controllers.actions.{AesAuthRequestActionBuilder, AesDataRequiredAction, AesDataRetrievalAction}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import uk.gov.hmrc.automatedexportsystemfrontend.views.html.$className$View
 
 class $className$Controller @Inject()(
                                        override val messagesApi: MessagesApi,
-                                       identify: IdentifierAction,
-                                       getData: DataRetrievalAction,
-                                       requireData: DataRequiredAction,
+                                       val actionBuilder: AesAuthRequestActionBuilder,
+                                       getData: AesDataRetrievalAction,
+                                       requireData: AesDataRequiredAction,
                                        val controllerComponents: MessagesControllerComponents,
                                        view: $className$View
                                      ) extends FrontendBaseController with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad: Action[AnyContent] = (actionBuilder andThen getData andThen requireData) {
     implicit request =>
       Ok(view())
   }
